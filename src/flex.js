223a8a2b80df664d0060e96b94e004d18f168fca
phihash <insharp0220@gmail.com>
2026-07-06 23:06:57 +0900

    flex message

---
+++ src/flex.js
@@ -0,0 +1,52 @@
+export function buildFortuneFlex(num, description) {
+  return {
+    type: "flex",
+    altText: `あなたの秘数は【${num}】です`,
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "数秘術鑑定",
+            weight: "bold",
+            size: "xs",
+            color: "#9B59B6",
+          },
+          {
+            type: "text",
+            text: `${num}`,
+            weight: "bold",
+            size: "5xl",
+            align: "center",
+            color: "#6C3483",
+            margin: "lg",
+          },
+          {
+            type: "text",
+            text: `秘数 ${num}`,
+            align: "center",
+            size: "sm",
+            color: "#888888",
+          },
+          {
+            type: "separator",
+            margin: "lg",
+          },
+          {
+            type: "text",
+            text: description,
+            wrap: true,
+            size: "sm",
+            margin: "lg",
+            color: "#333333",
+          },
+        ],
+        paddingAll: "20px",
+        backgroundColor: "#F8F4FC",
+      },
+    },
+  };
+}
